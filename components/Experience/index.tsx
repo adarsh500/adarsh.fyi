6e87370038680bbe565d08474d7c15629fbf850c
adarsh50 <adarshsulegai50@gmail.com>
2022-12-18 12:02:35 +0530

    feat: responsive pro max
    Signed-off-by: adarsh50 <adarshsulegai50@gmail.com>

--- components/Experience/index.tsx
+++ components/Experience/index.tsx
@@ -5,55 +5,69 @@ import Dukaan2 from 'icons/Dukaan2';
 const Experience = () => {
   const [expanded, setExpanded] = useState(false);
 
+  const expandSection = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className={styles.experience}>
-      <div className={styles.left}>
-        <div className={styles.logo}>
-          <Dukaan2 />
+    // <div className={styles.vertical}>
+    <div className={styles.card}>
+      <div className={expanded ? styles.experienceExpanded : styles.experience}>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Dukaan2 />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.header}>
+            <div className={styles.headerLeft}>
+              <p className={styles.primary}>Dukaan</p>
+              <p className={styles.primaryMobile}>Dukaan</p>
+              <p className={styles.secondary}>Frontend Developer Intern</p>
+            </div>
+            <div className={styles.headerRight}>
+              <p className={styles.primary}>Apr 2022 - June 2022</p>
+              <p className={styles.primaryMobile}>Apr '22 - June '22</p>
+              <p className={styles.secondary}>Bangalore, India</p>
+            </div>
+          </div>
+          <div className={styles.body}>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore aliquid earum deleniti, tenetur temporibus
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
+                praesentium,
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
+                praesentium,
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
+                praesentium,
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
+                praesentium,
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className={styles.right}>
-        <div className={styles.header}>
-          <div className={styles.headerLeft}>
-            <p className={styles.primary}>Dukaan</p>
-            <p className={styles.secondary}>Frontend Developer Intern</p>
-          </div>
-          <div className={styles.headerRight}>
-            <p className={styles.primary}>Apr 2022 - June 2022</p>
-            <p className={styles.secondary}>Bangalore, India</p>
-          </div>
-        </div>
-        <div className={styles.body}>
-          <ul>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
-              praesentium,
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
-              praesentium,
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
-              praesentium,
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
-              praesentium,
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              dolore aliquid earum deleniti, tenetur temporibus nihil quisquam
-              praesentium,
-            </li>
-          </ul>
-        </div>
+      <div>
+        <button className={styles.expand} onClick={expandSection}>
+          {!expanded ? 'expand' : 'collapse'}
+        </button>
       </div>
     </div>
+    // </div>
   );
 };
 
