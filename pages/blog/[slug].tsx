d3f78faeb6eaa999bc32118341c55e420c287de9
adarsh500 <adarshsulegai50@gmail.com>
2023-02-16 01:18:01 +0530

    feat: add blogs

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,6 +2,39 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
+import styles from './Blog.module.scss';
+
+const PostLayout = ({ post }: any) => {
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      {/* <article className="mx-auto max-w-2xl py-16"> */}
+      <article className={styles.container}>
+        <p className={styles.title}>{post.title}</p>
+        <time dateTime={post.date} className="text-sm text-slate-600">
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+        <div
+          className={styles.blogs}
+          dangerouslySetInnerHTML={{ __html: post.body.html }}
+        ></div>
+      </article>
+      {/* <div className="mb-6 text-center"></div>
+        <div className="mb-6 text-center">
+          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+          
+        </div>
+        <div
+          className="cl-post-body"
+        />
+      </article> */}
+    </main>
+  );
+};
+
+export default PostLayout;
 
 export async function getStaticPaths() {
   const paths = allPosts.map((post) => post.url);
@@ -19,34 +52,3 @@ export async function getStaticProps({ params }: any) {
     },
   };
 }
-
-const PostLayout = ({ post }: any) => {
-  return (
-    <>
-      {/* <Head>
-        <title>{post.title}</title>
-      </Head>
-      <article className="mx-auto max-w-2xl py-16">
-        <div className="mb-6 text-center">
-          <Link href="/">
-            <a className="text-center text-sm font-bold uppercase text-blue-700">
-              Home
-            </a>
-          </Link>
-        </div>
-        <div className="mb-6 text-center">
-          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-          <time dateTime={post.date} className="text-sm text-slate-600">
-            {format(parseISO(post.date), 'LLLL d, yyyy')}
-          </time>
-        </div>
-        <div
-          className="cl-post-body"
-          dangerouslySetInnerHTML={{ __html: post.body.html }}
-        />
-      </article> */}
-    </>
-  );
-};
-
-export default PostLayout;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,86 +1,11 @@
 import styles from './Blog.module.scss';
-import * as path from 'path';
-import fs from 'fs';
-import matter from 'gray-matter';
 import BlogCard from '@components/BlogCard';
 import readingTime from 'reading-time';
-
-// const index = (props: { posts: any }) => {
-//   const { posts } = props;
-
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.container}>
-//         <p className={styles.title}>Blogs</p>
-//         <div className={styles.blogs}>
-//           {posts.map((post: any) => (
-//             <>
-//               <BlogCard
-//                 {...post.frontMatter}
-//                 slug={post.url}
-//                 readingTime={post.readingTime}
-//               />
-//               <BlogCard
-//                 {...post.frontMatter}
-//                 slug={post.url}
-//                 readingTime={post.readingTime}
-//               />
-//               <BlogCard
-//                 {...post.frontMatter}
-//                 slug={post.url}
-//                 readingTime={post.readingTime}
-//               />
-//               <BlogCard
-//                 {...post.frontMatter}
-//                 slug={post.url}
-//                 readingTime={post.readingTime}
-//               />
-//             </>
-//           ))}
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default index;
-
-// export const getStaticProps = async () => {
-//   const postsDirectory = fs.readdirSync(path.join('posts'));
-
-//   const posts = postsDirectory.map((fileName) => {
-//     const fullPath = fs.readFileSync(path.join('posts', fileName), 'utf-8');
-//     const { data: frontMatter } = matter(fullPath);
-
-//     return {
-//       frontMatter,
-//       slug: '/blog/' + fileName.split('.')[0],
-//       readingTime: readingTime(fullPath).text,
-//     };
-//   });
-
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// };
-
 import Head from 'next/head';
-import Link from 'next/link';
 import { compareDesc, format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
 
-export async function getStaticProps() {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
-  return { props: { posts } };
-}
-
 export default function Blog({ posts }: any) {
-  console.log('blog posts', posts);
-
   return (
     <main className={styles.main}>
       <Head>
@@ -102,4 +27,11 @@ export default function Blog({ posts }: any) {
       </div>
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const posts = allPosts.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date));
+  });
+  return { props: { posts } };
 }
