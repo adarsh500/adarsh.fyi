1e811dd2685554d7de37355226a1ea4fe8eae3fd
Adarsh Sulegai <adarshsulegai50@gmail.com>
2024-01-20 15:31:12 +0530

    fix: homepage

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
-    <main className="flex-1 flex items-center justify-center break-words text-xl flex-col text-left mt-24 mb-20 text-light-primary dark:text-dark-primary mobile:py-12 mobile:px-4 mobile:my-0">
+    <main className="flex-1 flex items-center justify-center break-words text-xl flex-col text-left mt-16 mb-12 text-light-primary dark:text-dark-primary mobile:py-12 mobile:px-4 mobile:my-0">
       <div className="flex items-start justify-center flex-col">
         <p
-          className="text-6xl leading-snug font-extrabold bg-[length:400%_400%] animate-gradientBlur bg-foo transition-all duration-300 ease-in-out text-transparent bg-clip-text mobile:text-[36px]"
+          className="text-5xl leading-snug font-extrabold bg-[length:400%_400%] animate-gradientBlur bg-foo transition-all duration-300 ease-in-out text-transparent bg-clip-text mobile:text-[36px]"
           id="hero-text"
         >
           Adarsh Sulegai
